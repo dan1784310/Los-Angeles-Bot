b7fc0740585e5ce0c97b22714d22e8dcec3bf533
dan1784310 <dan1784310@gmail.com>
2026-09-15 21:44:15 +0100

    update bot

--- bot.py
+++ bot.py
@@ -2214,7 +2214,7 @@ async def send_llc_log(
 
     target_word = parts[1]
 
-       if len(target_word) < 4:
+    if len(target_word) < 4:
         now = datetime.datetime.now()
         date_str = now.strftime("%d/%m/%Y")
         time_str = now.strftime("%I:%M %p").lower()
