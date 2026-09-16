10cb0aa454c761202a52698f8df7334f8b5d1b18
dan1784310 <dan1784310@gmail.com>
2026-09-16 23:15:17 +0100

    update bot

--- ticket_views.py
+++ ticket_views.py
@@ -503,11 +503,9 @@ class TicketSelectMenu(ui.Select):
         
         super().__init__(
             placeholder='Select a ticket category...',
-            custom_id='ticket_select_category',
             min_values=1,
             max_values=1,
-            options=options,
-            custom_id='ticket_dropdown'
+            options=options
         )
         self.on_select = on_select
     
